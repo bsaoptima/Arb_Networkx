1663a7330dad03e45bd91816a17843d422f18db5
bsa <81991553+bsaoptima@users.noreply.github.com>
2022-08-31 11:02:04 +0100

    Create db.py

---
+++ db.py
@@ -0,0 +1,40 @@
+import sqlite3
+import json
+import uuid
+
+def init_db():
+    con = sqlite3.connect('streaming-markets.db')
+    cur = con.cursor()
+
+    cur.execute(''' CREATE TABLE IF NOT EXISTS Streaming
+                    (Id text, Symbol text, Interval text, Close real, High real, Low real, Timestamp integer) ''')
+    
+    con.commit()
+
+def log_db(message):
+    parsed = json.loads(message)
+
+    k = parsed['k']
+    id = str(uuid.uuid4().hex)
+    symbol = k['s']
+    interval = k['i']
+    close = float(k['c'])
+    high = float(k['h'])
+    low = float(k['l'])
+    time = int(k['t'])
+
+    print("log: ")
+    print("symbol: " + symbol)
+    print("interval: " +  interval)
+    print("close: " + str(close))
+    print("high: " + str(high))
+    print("low: " + str(low))
+    print("time: " + str(time))
+
+    row = (id, symbol, interval, close, high, low, time)
+    con = sqlite3.connect('streaming-markets.db')
+    cur = con.cursor()
+    cmd = "insert into Streaming (Id, Symbol, Interval, Close, High, Low, Timestamp) values (?, ?, ?, ?, ?, ?, ?)"
+    cur.execute(cmd, row)
+    con.commit()
+    con.close()
